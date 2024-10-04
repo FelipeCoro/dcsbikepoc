da4c47371def2b1e064f3f3b03cb4ebb76c967cc
riandcaar <109320836+riandcaar@users.noreply.github.com>
2024-10-04 12:16:11 -0500

    feat(login): enhance login screen UI and functionality
    - Adjusted the size of the text fields and login button for improved usability
    - Increased the font size and applied bold style to the "Login" button text
    - Added a "Don't have an account? Sign Up" text with clickable functionality, positioned at the bottom of the screen using a flexible spacer
    - Implemented loading spinner in the "Login" button to indicate ongoing login process
    - Added delay simulation of 3 seconds to mimic login process
    - Positioned the logo above the text fields and adjusted its size for better alignment
    - Updated button text to "Login" for clarity and consistency

--- app/src/main/java/com/felipecoronado/tcsbikepoc/ui/login/LoginScreen.kt
+++ app/src/main/java/com/felipecoronado/tcsbikepoc/ui/login/LoginScreen.kt
@@ -1,54 +1,120 @@
 package com.felipecoronado.tcsbikepoc.ui.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.felipecoronado.tcsbikepoc.R
+import com.felipecoronado.tcsbikepoc.ui.theme.GreenTCS
+import kotlinx.coroutines.delay
 
 
 @Composable
 fun LoginScreen(
     onLoginSuccess: () -> Unit
 ) {
+    var emailState by remember { mutableStateOf("") }
+    var passwordState by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
+
+
+    LaunchedEffect(isLoading) {
+        if (isLoading) {
+            delay(3000)
+            isLoading = false
+            onLoginSuccess()
+        }
+    }
+
     Column(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
-        val emailState = remember { mutableStateOf("") }
-        val passwordState = remember { mutableStateOf("") }
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo_white),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(200.dp)
+                .padding(15.dp)
+        )
 
         OutlinedTextField(
-            value = emailState.value,
-            onValueChange = { emailState.value = it },
+            value = emailState,
+            onValueChange = { emailState = it },
             label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
         )
 
         OutlinedTextField(
-            value = passwordState.value,
-            onValueChange = { passwordState.value = it },
+            value = passwordState,
+            onValueChange = { passwordState = it },
             label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth()
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth()
         )
 
-        Button(onClick = onLoginSuccess,
+        Spacer(modifier = Modifier.padding(bottom = 10.dp))
+
+        Button(
+            onClick = {
+                isLoading = true
+            },
             shape = RoundedCornerShape(16.dp),
-            modifier = Modifier.padding(top = 16.dp)) {
-            Text("Login success go to Calendar")
+
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .widthIn(min = 250.dp, max = 350.dp)
+                .height(50.dp),
+            enabled = !isLoading
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(modifier = Modifier.size(24.dp))
+            } else {
+                Text("Login", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            }
         }
+
+        Text(
+            text = "Don't have an account? Sign Up ",
+            color = GreenTCS,
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .clickable { }
+        )
+
+        Spacer(modifier = Modifier.padding(bottom = 80.dp))
+
     }
 }
